9b047a5640768439c9a70cf2fabdfba782776080
zaara-mk <zaara.khan@city.ac.uk>
2024-10-27 01:24:53 +0100

    Update task1.sql

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -1,17 +1,17 @@
 CREATE TABLE petPet (
-  petname ???,
-  owner ???,
-  species ???,
-  gender ???,
-  birth ???,
-  death ???
+  petname VARCHAR(20) NOT NULL PRIMARY KEY,
+  owner CARCHAR(45),
+  species VARCHAR(45),
+  gender ENUM('M','F'),
+  birth DATE,
+  death DATE
 );
 
 CREATE TABLE petEvent (
-  petname ???,
-  eventdate ???,
-  eventtype ???,
-  remark ???,
-  FOREIGN KEY ???,
-  PRIMARY KEY ???
+  petname VARCHAR(20),
+  eventdate DATE NOT NULL,
+  eventtype VARCHAR(15) NOT NULL,
+  remark VARCHAR(255),
+  FOREIGN KEY (petname) REFERENCES petPet(petname),
+  PRIMARY KEY (eventdate,eventtype)
 );
