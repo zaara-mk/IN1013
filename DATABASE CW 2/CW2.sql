11ddf7bd29eaaa628651f50296c7aa6ed8b356d8
zaara-mk <zaara.khan@city.ac.uk>
2025-01-11 03:31:13 +0000

    Create CW2.sql

---
+++ DATABASE CW 2/CW2.sql
@@ -0,0 +1,112 @@
+/*
+Student name: Zaara Khan             
+Student ID: 230001244
+*/
+
+/* SECTION 1 - CREATE TABLE STATEMENTS */
+
+
+
+/* SECTION 2 - INSERT STATEMENTS */
+
+
+                     
+/* SECTION 3 - UPDATE STATEMENTS - The queries must be explained in natural (English) language first, and then followed up by respective statements */
+
+/*
+1)
+
+*/
+
+/*
+2)
+
+*/
+
+
+/* SECTION 4 - SELECT STATEMENTS - The queries must be explained in natural (English) language first, and then followed up by respective SELECTs*/
+
+
+/* 
+1)  
+
+*/
+select '1)' AS '';
+
+
+
+/* 
+2)  
+
+*/
+select '2)' AS '';
+
+
+
+
+/* 
+3)  
+
+*/
+select '3)' AS '';
+
+
+
+/* 
+4)  
+
+*/
+select '4)' AS '';
+
+
+
+
+/* 
+5)  
+
+*/
+select '5)' AS '';
+
+
+
+/* 
+6)  
+
+*/
+select '6)' AS '';
+
+
+/* 
+7)  
+
+*/
+select '7)' AS '';
+
+
+
+/* 
+8)  
+
+*/
+select '8)' AS '';
+
+
+
+/* SECTION 5 - DELETE ROWS - The queries must be explained in natural (English) language first, and then followed up by respective statements */
+
+/*
+1)
+
+*/
+
+/*
+2)
+
+*/
+
+
+/* SECTION 6 - DROP TABLES */
+
+
+
+SHOW TABLES;
